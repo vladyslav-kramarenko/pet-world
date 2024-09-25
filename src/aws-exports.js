ec5172dc7b4ae740dce59ee4651c1dbd33b2aeba
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-09-24 18:47:31 -0700

    added stub for aws exports

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsmobile = {
+    aws_project_region: "your-region",
+    aws_cognito_region: "your-region",
+    aws_user_pools_id: "your-cognito-user-pool-id",
+    aws_user_pools_web_client_id: "your-cognito-app-client-id",
+    oauth: {}
+};
+
+export default awsmobile;
