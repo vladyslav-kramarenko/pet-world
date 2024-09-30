3e5ebf0342533d622788b5cea46fabadebc85f28
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-09-30 12:13:07 -0700

    set basic routes, commented out authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+// import { Amplify } from 'aws-amplify';
+// import { Authenticator } from '@aws-amplify/ui-react';
+// import '@aws-amplify/ui-react/styles.css';  // Import the UI styles
+import awsconfig from './aws-exports';
+// import {Router} from "@aws-amplify/ui-react/dist/types/components/Authenticator/Router";
+import PetList from "./pages/PetList";
+import {BrowserRouter as Router, Navigate, Route, Routes} from 'react-router-dom'; //
+import PetProfile from "./pages/PetProfile";
+import CreatePet from "./pages/CreatePet";
+import EditPet from "./pages/EditPet";
+
+
+// Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+  //     <Authenticator>
+  //       {({ signOut, user }) => (
+  //           <main>
+  //             <h1>Welcome, {user?.username}</h1>
+  //             <button onClick={signOut}>Sign out</button>
+  //           </main>
+  //       )}
+  //     </Authenticator>
+  // );
+    return (
+        <Router>
+            <Routes>
+                {/* Route for the home page which lists all pets */}
+                <Route path="/" element={<PetList />} />
+
+                {/* Route for viewing a specific pet's profile */}
+                <Route path="/pet/:pet_id" element={<PetProfile />} />
+
+                {/* Route for creating a new pet */}
+                <Route path="/create" element={<CreatePet />} />
+
+                {/* Route for editing an existing pet */}
+                <Route path="/edit/:pet_id" element={<EditPet />} />
+
+                {/* Redirect to the home page if the route doesn't exist */}
+                <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
