16dfb1570c78bf8d5463aef865f5020f309a962d
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-10-06 09:09:35 -0700

    added multiple images to pet type

--- src/types/Pet.ts
+++ src/types/Pet.ts
@@ -12,6 +12,7 @@ export type Pet = {
     gender?: string;          // Gender of the pet (e.g., male, female, unknown)
     health_status: string[];  // Health status indicators (e.g., sterilized, vaccinated) - Array of strings
     documents: string[];      // Documents related to the pet (e.g., Breed Certificate, Vet Passport) - Array of strings
-    image_url?: string;       // URL of the pet's image
+    main_image_url?: string;       // URL of the pet's image
+    images?: string[];
     description?: string;     // Additional description for the pet
 };
