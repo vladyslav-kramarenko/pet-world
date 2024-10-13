901a4ba7b696862dd79720749af06c95b1142a77
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-10-13 14:16:14 -0700

    data update

--- src/types/Pet.ts
+++ src/types/Pet.ts
@@ -1,8 +1,9 @@
 export type Pet = {
-    pet_id: string;           // Unique identifier for each pet
+    pet_id?: string;           // Unique identifier for each pet
     pet_name: string;         // Name of the pet
     pet_type: string;         // Type of pet (e.g., dog, cat)
-    age: string;              // Age category (e.g., young, adult, senior)
+    exact_age?: string;              // Age category (e.g., young, adult, senior)
+    age_category: string;
     country: string;          // Country where the pet is located
     province: string;         // Province or state where the pet is located
     town: string;             // City or town where the pet is located
@@ -10,9 +11,17 @@ export type Pet = {
     longitude?: number;       // Longitude for geolocation (optional)
     price?: number;           // Price of the pet
     gender?: string;          // Gender of the pet (e.g., male, female, unknown)
-    health_status: string[];  // Health status indicators (e.g., sterilized, vaccinated) - Array of strings
-    documents: string[];      // Documents related to the pet (e.g., Breed Certificate, Vet Passport) - Array of strings
-    main_image_url?: string;       // URL of the pet's image
-    images?: string[];
+    main_image_url?: string;  // URL of the pet's image
+    images: string[];
     description?: string;     // Additional description for the pet
+    contact_phone: string;
+    contact_name: string;
+    owner_id: string;
+    "isSterilized": false,
+    "isVaccinated": false,
+    "hasChip": false,
+    "hasParasiteTreatment": false,
+    "hasVetPassport": false,
+    "hasPedigree": false,
+    "hasFCICertificate": false,
 };
