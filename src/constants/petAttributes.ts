42630a76bb940b5676290f09659ed3b8431ea587
vladyslav-kramarenko <razorvlad1992@gmail.com>
2024-10-13 22:07:57 -0700

    cleaning

--- src/constants/petAttributes.ts
+++ src/constants/petAttributes.ts
@@ -1,15 +1,4 @@
 export const PET_TYPES: string[] = ['Dog', 'Cat', 'Bird', 'Rabbit', 'Other'];
 
-export const PET_AGES: { value: number; label: string }[] = [
-    { value: 0, label: 'Less than 1 year' },
-    { value: 1, label: '1 year' },
-    { value: 2, label: '2 years' },
-    { value: 3, label: '3 years' },
-    { value: 4, label: '4 years' },
-    { value: 5, label: '5 years' },
-    { value: 6, label: '6 years' },
-    { value: 7, label: '7+ years' },
-];
-
 export const AGE_CATEGORIES = ['Baby','Young', 'Adult', 'Senior'];
 
